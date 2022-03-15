c322751c734f76092b8b62e8f3b36a190e26bd05
walid0124 <98133034+walid0124@users.noreply.github.com>
2022-03-15 23:12:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Append SUM:
+L1=[1,1,2]
+print ("This is your list: " + str(L1))
+num1= 1
+num2= 2
+sum= [1 + 2]
+for x in sum:
+  print (x)
+num1= 2
+num2= 3
+sum= [2 + 3]
+for x in sum:
+  print (x)
+num1= 3
+num2= 5
+sum= [3 + 5]
+for x in sum:
+  print (x)
